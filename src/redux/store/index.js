3e634e30b2ededce5c6187d436f8ce1d77118bcd
Daniel <dan.earp@icloud.com>
2022-12-06 16:02:05 +0000

    Add LocalStorage

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,14 +1,27 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import searchReducer from '../reducer/searchReducer'
 import favouriteReducer from '../reducer/favouriteReducer'
+import localStorage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
 
 export const combinedReducer = combineReducers({
   mainSearchJobs: searchReducer,
   favouriteJobs: favouriteReducer,
 })
 
-const store = configureStore({
-  reducer: combinedReducer,
+const persistConfig = {
+  key: 'root',
+  storage: localStorage,
+}
+
+const mainReducer = persistReducer(persistConfig, combinedReducer)
+
+export const store = configureStore({
+  reducer: mainReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
 
-export default store
+export const persistor = persistStore(store)
